ae3b277d31d6c8d716c21a7790f173e838d03271
Derek Shoemaker <shoemakerdr@gmail.com>
2017-12-30 21:51:14 -0600

    Implemented die face image exercise

--- examples/04-random.elm
+++ examples/04-random.elm
@@ -1,9 +1,10 @@
 import Html exposing (..)
+import Html.Attributes exposing (..)
 import Html.Events exposing (..)
 import Random
 
 
-
+main : Program Never Model Msg
 main =
   Html.program
     { init = init
@@ -18,13 +19,13 @@ main =
 
 
 type alias Model =
-  { dieFace : Int
+  { dieImg : String
   }
 
 
 init : (Model, Cmd Msg)
 init =
-  (Model 1, Cmd.none)
+  (Model "https://www.wpclipart.com/recreation/games/dice/die_face_1.png", Cmd.none)
 
 
 
@@ -43,8 +44,15 @@ update msg model =
       (model, Random.generate NewFace (Random.int 1 6))
 
     NewFace newFace ->
-      (Model newFace, Cmd.none)
+      let
+        url = dieFaceUrl newFace
+      in
+        (Model url, Cmd.none)
 
+
+dieFaceUrl : Int -> String
+dieFaceUrl num =
+  "https://www.wpclipart.com/recreation/games/dice/die_face_" ++ toString num ++ ".png"
 
 
 -- SUBSCRIPTIONS
@@ -62,6 +70,8 @@ subscriptions model =
 view : Model -> Html Msg
 view model =
   div []
-    [ h1 [] [ text (toString model.dieFace) ]
-    , button [ onClick Roll ] [ text "Roll" ]
+    [ img [src model.dieImg, alt "die face", title <| "Die showing " ++ toString 1 ] []
+    , div []
+      [ button [ style [("padding", "20px"),("font-size", "24px"), ("margin", "16px")], onClick Roll ] [ text "Roll" ]
+      ]
     ]
